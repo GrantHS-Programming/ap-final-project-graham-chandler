f3b413d44778815c1f98618198e45c7b24f2f067
gchandler6995 <fe@gmail.com>
2024-06-13 11:31:08 -0700

    done today

--- AP3.java
+++ AP3.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class AP3 {
@@ -63,7 +65,7 @@ public class AP3 {
                                 System.out.println("(1) Cat   (2) Parrot   (3) Hamster");
                                 String me2 = answer.next();
                                 if(me2.equals("Cat")){
-                                    System.out.println("Meet Gato - She is a Maine Coo, 7 years old, and 10 pounds.");
+                                    System.out.println("Meet Gato - She is a Maine Coon, 7 years old, and 10 pounds.");
                                 }
                             }
                             if (otheranimals2.equals("no")){
@@ -71,8 +73,71 @@ public class AP3 {
                             }
                         }
                     }
+                    if(play2.equals("adopt")){
+                        System.out.println("Congratulations, Pero is your new dog!");
+                    }
+                    if(play2.equals("meet other animals")){
+                        System.out.println("Would you like to meet a parrot, hamster or cat?");
+                        String cat = answer.next();
+                        if(cat.equals("cat")){
+                            System.out.println("Meet Gato");
+                        }
+                        if(cat.equals("Hamster")){
+                            System.out.println("Meet Bert the hamster");
+                        }
+                    }
                 }
             }
+            if(num.equals("2")){
+                System.out.println("Meet Gato - She is a Maine Coon, 7 years old and 10 pounds.");
+                System.out.println("Would you like to play, listen, or adopt Gato?");
+                String p = answer.next();
+                if(p.equals("play")){
+                    System.out.println("Gato plays with the string.");
+                    System.out.println("Would you like to listen, or adopt Gato?");
+                    String li = answer.next();
+                    if(li.equals("listen")){
+                        System.out.println("MEOW MEOW MEOW");
+                        System.out.println("Would you like to adopt Gato?");
+                        String a = answer.next();
+                        if(a.equals("yes")){
+                            System.out.println("Congratulations, Gato is your new Cat!");
+                            System.out.println("Would you like to meet other pets?");
+                            String op = answer.next();
+                            if(op.equals("yes")){
+                                System.out.println("Would you like to meet (1) Dog, (2) Parrot, (3) Hamster");
+                                String meet = answer.next();
+                                if(meet.equals("Parrot")){
+                                    System.out.println("Meet Rico - He is a grey parrot, 3 years old, 4 pounds.");
+                                    System.out.println("DO you want to play, listen or adopt Rico?");
+                                    String add = answer.next();
+                                    if(add.equals("play")){
+                                        System.out.println("Rico flies around your head");
+                                        System.out.println("Do you want to listen or adopt Rico?");
+                                        String z = answer.next();
+                                        if(z.equals("listen")){
+                                            System.out.println("CHIRP CHIRP CHIRP");
+                                            System.out.println("Do you want to adopt Rico?");
+                                            String w = answer.next();
+                                            if(w.equals("yes")){
+                                                System.out.println("Congratualations, Rico is your new Parrot!");
+                                                System.out.println("Would you like to meet more animals?");
+                                                String mmmmm = answer.next();
+                                                if(mmmmm.equals("no")){
+                                                    System.out.println("Goodbye!");
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if(yes.equals("no")){
+            System.out.println("Goodbye!");
         }
     }
 }
